bcdcf477bb6e7b907b844c34ab0e251d9d74320e
SergeyKorolenko <pavilion1900@gmail.com>
2026-05-15 22:32:34 +0300

    feature(): init commit

---
+++ src/main/java/org/job/App.java
@@ -0,0 +1,8 @@
+package org.job;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
---
+++ src/test/java/org/job/AppTest.java
@@ -0,0 +1,10 @@
+package org.job;
+
+import org.junit.jupiter.api.Test;
+
+class AppTest {
+
+    @Test
+    void appHasAGreeting() {
+    }
+}
